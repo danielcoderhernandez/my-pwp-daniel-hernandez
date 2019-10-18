28488de5000b226c20f06da70982a675e65ac5f0
Daniel Hernandez <dhernandez43@cnm.edu>
2019-10-17 22:57:24 -0600

    updated persona

--- public_html/directory/milestone-1.php
+++ public_html/directory/milestone-1.php
@@ -4,34 +4,41 @@
 	<meta charset="UTF-8">
 	<title>Purpose, Audience, Goal</title>
 	<link rel="stylesheet" href="./directory-style.css">
-	<style>
-		#myDIV {width: 97.25%; height: 100px;  background: black; -webkit-animation: mymove 5s ease-in-out;
-			animation: mymove 5s ease-in-out;}
-		@-webkit-keyframes {from {background-color: lime;} to {background-color: black;}}
-		@keyframes mymove { from {background-color: lime;} to {background-color: black;} }
-		}
-	</style>
 </head>
 
-<body id="myDIV">
-	<p> Purpose: To Display my Portfolio.</p>
+<body>
 
-	<P> Audience: </P>
+	<h1> Purpose: To Display my Portfolio.</h1>
+
+	<h2> Audience: </h2>
 		<p> 1. Clients looking for custom websites </p>
 		<p> 2. Businesses looking to subcontract work </p>
 		<p> 3. Peers looking to collaborate </P>
 
-	<p> Goal: </p>
+	<h2> Goal: </h2>
 		<p> 1. To gain freelance work</p>
 		<p> 2. To gain subcontracted work from businesses building websites</p>
 		<p> 3. To network with local, national, and international peers and collaborate on projects</p>
 
-	<p> Persona: </p>
+	<h2> Persona: </h2>
+	<p></p>
 	<p> Name, Aaron Lee</p>
-	<p> Aaron is a minority of Native Ancestry who is looking to get bookings for his band Spare Change.</p>
-	<p> He currently promotes his band over Facebook and Instagram, but the social-media sites are limiting </p>
-	<p> and are not displaying the band's songs or work as well as he would like.</p>
-	<p> He is looking for someone to make a custom website that will feature the band's members, songs, past performances,</p>
-	<p> and allow any potential clients to request a booking. </p>
+	<p></p>
+	<p> Age 31, Race: Native</p>
+	<p></p>
+	<p> Attitude and Behaviors: Aaron has a full time job in the day which severely limits the amount of time he can promote his band Spare Change</p>
+	<p> He currently promotes his band over Facebook and Instagram.</p>
+
+	<p> Technology: His main on person device is his Samsung Note 9 with the latest software. He regularly checks the band's Facebook and Instagram to see if anyone is attempting
+	to request a booking. He also has a HP Windows desktop at home that runs Windows10 where he stores video and songs of the band Spare Change</p>
+
+	<p> Frustrations: Aaron believes that the social-media sites are not fully displaying the bands work and information as well as he would like which is
+		limiting the amount of bookings the band is getting</p>
+
+	<p> Goals: Aaron is looking to increase bookings for his band by hiring a front-end developer that can better display the bands members and performances.</p>
+	<p> and a page that allows any potential clients to request a booking while linking the band's social-media sites. </p>
+
+	<p> User Story: As an artist looking to increase bookings for my band I am looking for a front-end developer that can make a quality-interactive site that my
+	potential clients </p>
 
 </body>
