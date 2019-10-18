dfbb27ec54273357eeae100fad94c1991ccf6353
Daniel Hernandez <dhernandez43@cnm.edu>
2019-10-18 09:33:10 -0600

    updated h tags and clean the layout

--- public_html/directory/milestone-1.php
+++ public_html/directory/milestone-1.php
@@ -8,37 +8,56 @@
 
 <body>
 
-<p> <b> Purpose: To Display my Portfolio. </b> </p>
+<h1> <b> Purpose: To Display my Portfolio </b> </h1>
+<p></p>
 
-<p> <b> Audience: </b> </p>
+<h2> <b> Audience: </b> </h2>
 		<p> 1. Clients looking for custom websites </p>
 		<p> 2. Businesses looking to subcontract work </p>
 		<p> 3. Peers looking to collaborate </P>
+		<p></p>
 
-<p> <b> Goal: </b> </p>
+<h2> <b> Goal: </b> </h2>
 		<p> 1. To gain freelance work</p>
 		<p> 2. To gain subcontracted work from businesses building websites</p>
 		<p> 3. To network with local, national, and international peers and collaborate on projects</p>
+		<p></p>
 
-<p> <b> Persona: </b> </p>
+<h2> <b> Persona: </b> </h2>
 	<p></p>
 	<p> Name, Aaron Lee</p>
 	<p></p>
 	<p> Age 31, Race: Native</p>
 	<p></p>
-	<p> Attitude and Behaviors: Aaron has a full time job in the day which severely limits the amount of time he can promote his band Spare Change</p>
-	<p> He currently promotes his band over Facebook and Instagram.</p>
+	<p><b> Attitude and Behaviors:<b/></p>
+	<p>Aaron has a full time job in the day which severely limits the amount of time he can promote his band Spare Change</p>
+	<p>He currently promotes his band over Facebook and Instagram.</p>
+	<p></p>
 
-	<p> Technology: His main on person device is his Samsung Note 9 with the latest software. He regularly checks the band's Facebook and Instagram to see if anyone is attempting
-	to request a booking. He also has a HP Windows desktop at home that runs Windows10 where he stores video and songs of the band Spare Change</p>
+<p><b> Technology: </b></p>
+<p></p>
+	<p>His main on person device is his Samsung Note 9 with the latest software. He regularly checks the band's</p>
+	<p>Facebook and Instagram to see if anyone is attempting</p>
+	<p>to request a booking. He also has a HP Windows desktop at home that runs Windows10 where he stores video and songs </p>
+	<p>of the band Spare Change</p>
+	<p></p>
 
-	<p> Frustrations: Aaron believes that the social-media sites are not fully displaying the band's work and information as well as he would like which is
-		limiting the amount of bookings the band is getting</p>
+	<p> <b> Frustrations: </b> </p>
+	<p></p>
+	<p>Aaron believes that the social-media sites are not fully displaying the band's work and information as well </p>
+	<p>as he would like which is limiting the amount of bookings the band is getting</p>
 
-	<p> Goals: Aaron is looking to increase bookings for his band by hiring a front-end developer that can better display the band's members and performances.</p>
-	<p> and a page that allows any potential clients to request a booking while linking the band's social-media sites. </p>
+<p><b> Goals:</b></p>
+	<p></p>
+	<p>Aaron is looking to increase bookings for his band by hiring a front-end developer that can better display the
+	<p>band's members and performances and a page that allows any potential clients to request a booking while linking </p>
+	<p>the band's social-media sites. </p>
 	<p> Since he works full time he wants to quickly review a developer's portfolio and contact information </p>
-	<p> User Story: As an artist looking to increase bookings for my band I am looking for a front-end developer that can make a quality-interactive site that my
-	potential clients </p>
+	<p></p>
+
+<p> <b> User Story: </b> </p>
+	<p></p>
+	<p>As an artist looking to increase bookings for my band I am looking for a front-end developer that can make a </p>
+	<p> quality-interactive site that my potential clients </p>
 
 </body>
