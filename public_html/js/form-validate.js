db6398c5e11877fd89ad2286f508131c47dabb03
Daniel Hernandez <dhernandez43@cnm.edu>
2019-11-12 14:02:51 -0700

    working on getting php index to ref js file4

--- public_html/js/form-validate.js
+++ public_html/js/form-validate.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 	 **/
 
 	/* begin validate function here */
-	$("#pwp-form").validate({
+	$("#form").validate({
 
 		// setup handling of form errors
 		debug: true,
@@ -55,9 +55,9 @@ $(document).ready(function(){
 
 		// AJAX submit the form data to back end if rules pass
 		submitHandler: function(form) {
-			$("#pwp-form").ajaxSubmit({
+			$("#form").ajaxSubmit({
 				type: "POST",
-				url: $("#pwp-form").attr("action"),
+				url: $("#form").attr("action"),
 
 				success: function(ajaxOutput) {
 					// clear the output area's formatting
@@ -68,7 +68,7 @@ $(document).ready(function(){
 
 					// reset the form if it was successful
 					if($(".alert-success").length >= 1) {
-						$("#pwp-form")[0].reset();
+						$("#form")[0].reset();
 					}
 				}
 			})
