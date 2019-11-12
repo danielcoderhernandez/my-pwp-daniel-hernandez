ec4495a3a570c981e871b603b43ff7e56a5801f8
Daniel Hernandez <dhernandez43@cnm.edu>
2019-11-12 14:02:26 -0700

    working on getting php index to ref js file4

--- public_html/index.php
+++ public_html/index.php
@@ -17,12 +17,12 @@
 			  crossorigin="anonymous"></script>
 
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+<!--	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
 			  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
 			  crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
 			  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-			  crossorigin="anonymous"></script>
+			  crossorigin="anonymous"></script> -->
 
 	<!-- jQuery Form, Additional Methods, Validate -->
 
@@ -116,7 +116,7 @@ Begin Sample Contact Form
 Create your own contact form.
 Do not copy and paste this one.
 -->
-	<form id="pwp-form" action="../php/" method="post">
+	<form id="form" action="../php/" method="post">
 		<div class="form-group">
 			<label for="contactDemoName">Name <span class="text-danger">*</span></label>
 			<div class="input-group">
