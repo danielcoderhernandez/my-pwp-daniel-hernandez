a6ef0ef4d9114c183dc180675b947ac4c04b2957
Daniel Hernandez <dhernandez43@cnm.edu>
2019-11-18 20:39:17 -0700

    added practice navbar

---
+++ Practice/bootstrap-navbar.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+			integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<!--<link rel="stylesheet" id="theme_link" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/materia/bootstrap.min.css">-->
+
+	<link rel="stylesheet" href="navbar-fixed-left.min.css">
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+			  integrity="sha384-vk5WoKIaW/vJyUAd9n/wmopsmNhiy+L2Z+SBxGYnUkunIxVxAv/UtMOhba/xskxh"
+			  crossorigin="anonymous"></script>
+
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+			  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+			  crossorigin="anonymous"></script>
+
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+			  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+			  crossorigin="anonymous"></script>
+
+	<title> Practice Navbar BootStrap</title>
+</head>
+<body>
+
+	<section>
+
+		<nav class="navbar navbar-expand-md navbar-light" style="background-color: navajowhite">
+				<h1 class="navbar-brand mx-auto"> Daniel Hernandez </h1>
+				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+						  data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+		</nav>
+
+		<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+			<ul class="navbar-nav mr-auto mt-2 mt-md-0">
+				<li class="nav-item active">
+					<a class="nav-link" href="bootstrap-navbar.php"> About <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="bootstrap-navbar.php"> Portfolio </a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="bootstrap-navbar.php"> Contact </a>
+				</li>
+			</ul>
+		</div>
+
+	</section>
+
+</body>
+
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+			  aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<a class="navbar-brand" href="#!">Navbar</a>
+
+	<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+		<ul class="navbar-nav mr-auto mt-2 mt-md-0">
+			<li class="nav-item active">
+				<a class="nav-link" href="#!">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#!">Link</a>
+			</li>
+		</ul>
+	</div>
+</nav>
